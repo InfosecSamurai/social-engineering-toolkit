be2afdd4dd8c3f88590701857b4edb09c92088c4
InfosecSamurai <William.B.Shaw@proton.me>
2025-11-28 16:20:06 -0500

    Update remove_persistence.sh

--- attacks/usb_drop/advanced/cleanup_tools/remove_persistence.sh
+++ attacks/usb_drop/advanced/cleanup_tools/remove_persistence.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 # Removes persistence mechanisms
+
+# Remove all cron jobs (use with caution)
+# Use specific command to target malicious jobs if possible
 crontab -r
-systemctl stop malicious-service 2>/dev/null
-rm -f /etc/systemd/system/malicious-service.service
+
+# Check if the malicious service is running before stopping it
+if systemctl is-active --quiet malicious-service; then
+    systemctl stop malicious-service
+fi
+
+# Remove the service file if it exists
+if [ -f /etc/systemd/system/malicious-service.service ]; then
+    rm -f /etc/systemd/system/malicious-service.service
+fi
